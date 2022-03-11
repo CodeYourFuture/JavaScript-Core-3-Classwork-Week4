6c254c4ea3b2572c272e023db793d4411bd04db0
sampennington <samueljpennington64+1@gmail.com>
2022-03-11 16:08:03 +0000

    rename folder

--- exercises/A-make-tests-pass/mandatory/01-add-one/add-one.test.js
+++ exercises/A-make-tests-pass/mandatory/01-add-one/add-one.test.js
@@ -2,13 +2,11 @@
 
 const addOne = require("./add-one.js");
 
-test("Add 1 to each item in myArray", () => {
+test("Adds 1 to each item in the array", () => {
   const myArray = [31, 57, 12, 5];
-
-  const unchanged = [31, 57, 12, 5];
   const expected = [32, 58, 13, 6];
+
   const output = addOne(myArray);
 
   expect(output).toEqual(expected);
-  expect(myArray).toEqual(unchanged);
 });
--- exercises/A-make-tests-pass/mandatory/02-get-word-lengths/get-word-lengths.test.js
+++ exercises/A-make-tests-pass/mandatory/02-get-word-lengths/get-word-lengths.test.js
@@ -2,10 +2,11 @@
 
 const getWordLengths = require("./get-word-lengths");
 
-test("Get word lengths", () => {
+test("Returns the word lengths", () => {
   const words = ["sun", "potato", "roundabout", "pizza"];
   const expected = [3, 6, 10, 5];
 
   const output = getWordLengths(words);
+
   expect(output).toEqual(expected);
 });

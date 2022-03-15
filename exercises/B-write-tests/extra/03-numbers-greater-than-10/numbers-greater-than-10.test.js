2335100b4ad5ec5aef466a6052da1db53b30a175
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-03-15 18:24:10 +0000

    Add run-tests.yml

--- exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
+++ exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
@@ -3,7 +3,7 @@ const numbersGreaterThan10 = require("./numbers-greater-than-10");
 /* Complete the tests */
 
 test("Get numbers greater than 10", () => {
-  // Arrange
-  // Act
-  // Assert
+  const result = numbersGreaterThan10([1, 5, 10, 12, 100]);
+
+  expect(result).toEqual([12, 100]);
 });
--- exercises/B-write-tests/mandatory/01-greet-person/greet-person.test.js
+++ exercises/B-write-tests/mandatory/01-greet-person/greet-person.test.js
@@ -3,7 +3,7 @@ const greetPerson = require("./greet-person");
 /* Complete the tests */
 
 test("return person's name prefixed with Hello", () => {
-  // Arrange
-  // Act
-  // Assert
+  const result = greetPerson("Etza");
+
+  expect(result).toEqual("Hello Etza");
 });

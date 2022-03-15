2aa25c9bfb055184ea62aa8df7032be1d9165e1c
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-03-15 12:57:22 +0000

    exercise find-needle done

--- exercises/A-make-tests-pass/extra/04-find-needle/find-needle.js
+++ exercises/A-make-tests-pass/extra/04-find-needle/find-needle.js
@@ -1,3 +1,4 @@
-// module.exports = function findNeedle(words, needle) {
-//   /* Implement the function to make the tests pass */
-// }
+module.exports = function findNeedle(words, needle) {
+  /* Implement the function to make the tests pass */
+  return words.findIndex((e) => e === needle);
+};
--- exercises/A-make-tests-pass/extra/04-find-needle/find-needle.test.js
+++ exercises/A-make-tests-pass/extra/04-find-needle/find-needle.test.js
@@ -1,19 +1,19 @@
 /* Do not change this file */
 
-// const findNeedle = require("./find-needle");
+const findNeedle = require("./find-needle");
 
-// test("Find the needle", () => {
-//   const words = ["house", "train", "slide", "needle", "book"];
-//   const expected = 3;
+test("Find the needle", () => {
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
 
-//   const output = findNeedle(words, "needle");
-//   expect(output).toEqual(expected);
-// });
+  const output = findNeedle(words, "needle");
+  expect(output).toEqual(expected);
+});
 
-// test("Find the plant", () => {
-//   const words = ["plant", "shelf", "arrow", "bird"];
-//   const expected = 0;
+test("Find the plant", () => {
+  const words = ["plant", "shelf", "arrow", "bird"];
+  const expected = 0;
 
-//   const output = findNeedle(words, "plant");
-//   expect(output).toEqual(expected);
-// });
+  const output = findNeedle(words, "plant");
+  expect(output).toEqual(expected);
+});

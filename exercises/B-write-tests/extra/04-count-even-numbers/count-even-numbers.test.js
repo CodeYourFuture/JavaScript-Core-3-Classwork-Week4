c08f95d61b70925e1e82df09e311594f307414de
Daniel <pigos487@gmail.com>
2022-03-16 14:41:25 +0000

    tests action

--- exercises/B-write-tests/extra/04-count-even-numbers/count-even-numbers.test.js
+++ exercises/B-write-tests/extra/04-count-even-numbers/count-even-numbers.test.js
@@ -1,15 +1,9 @@
 const countEvenNumbers = require("./count-even-numbers");
 
-/* Complete the tests */
-
 test("Should return count of even numbers if there is at least one", () => {
-  // Arrange
-  // Act
-  // Assert
+  expect(countEvenNumbers([1, 2, 3, 4, 5, 6])).toEqual(`Even number count: 3`);
 });
 
 test("Should return message if there are no even numbers", () => {
-  // Arrange
-  // Act
-  // Assert
+  expect(countEvenNumbers([1, 3, 5])).toEqual("There are no even numbers");
 });

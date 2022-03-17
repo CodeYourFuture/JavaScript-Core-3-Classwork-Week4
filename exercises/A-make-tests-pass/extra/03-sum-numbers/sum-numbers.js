5c150dc28209016a2ad8755ddcfa66918564db46
Hatice06 <haticeaydogan00@gmail.com>
2022-03-17 21:54:32 +0000

    testing github actions

--- exercises/A-make-tests-pass/extra/03-sum-numbers/sum-numbers.js
+++ exercises/A-make-tests-pass/extra/03-sum-numbers/sum-numbers.js
@@ -1,10 +1,10 @@
 module.exports = function sumNumbers(numbers) {
   /* Implement the function to make the tests pass */
-  let sum = 0;
+  let total = 0;
   for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
+    total += numbers[i];
   }
-  return sum;
+  return total;
 }
 
 

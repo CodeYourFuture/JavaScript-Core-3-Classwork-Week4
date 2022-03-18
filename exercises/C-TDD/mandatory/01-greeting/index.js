6e17eedf6f70ea5cdec5f34e186759be8b114164
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-03-18 16:42:16 +0000

    complete th task

--- exercises/C-TDD/mandatory/01-greeting/index.js
+++ exercises/C-TDD/mandatory/01-greeting/index.js
@@ -1,2 +1,10 @@
-const greet = () => {};
+const greet = (name) => {
+  return name instanceof Array
+    ? (`Hello, ` + name.join(`, `)).replace(/,(?=[^,]*$)/, " and") + "."
+    : name === `nulls`
+    ? "Hello, my friend."
+    : name === name.toUpperCase()
+    ? `HELLO, ${name}`
+    : `Hello, ${name}`;
+};
 module.exports = greet;

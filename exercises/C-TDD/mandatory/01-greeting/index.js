145ce871dfbd997e626b4eb071aacaf72bc1e781
Hamra <hamra.khan.b@gmail.com>
2022-03-18 01:30:48 +0000

    completed mandatory exercises A and C

--- exercises/C-TDD/mandatory/01-greeting/index.js
+++ exercises/C-TDD/mandatory/01-greeting/index.js
@@ -1,1 +1,16 @@
-const greet = () => {}
+const greet = (name) => {
+    if (name === "Bob") {
+      return 'Hello, ' + name;
+    } else if (name === null) {
+      return 'Hello, my friend';
+    } else if (!Array.isArray(name) && name.toUpperCase()) {
+      return "HELLO " + name + "!";
+    } else if (Array.isArray(name)) {
+        let names = name.join(", ");
+        const searchTerm = ",";
+        let lastIndex = names.lastIndexOf(searchTerm);
+      return "Hello, " + names.substring(0, lastIndex) + " and" + names.substring(lastIndex + 1);
+    } 
+}
+
+module.exports = greet;

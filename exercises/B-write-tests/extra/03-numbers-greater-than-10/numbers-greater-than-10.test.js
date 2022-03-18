b98ef3ceba934662cfa75c4aa5d91127d8bd06e6
AmarachiOkereke <90873234+AmarachiOkereke@users.noreply.github.com>
2022-03-18 10:35:09 +0000

    All erexcises completed

--- exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
+++ exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
@@ -3,7 +3,8 @@ const numbersGreaterThan10 = require("./numbers-greater-than-10");
 /* Complete the tests */
 
 test("Get numbers greater than 10", () => {
-  const numbers = [1, 5, 10, 12, 100];
-  const result = numbersGreaterThan10(numbers);
-  expect(result).toEqual()
+  const numbersArray = [1, 5, 10, 12, 100];
+  const result = numbersGreaterThan10(numbersArray);
+  const expected = [12, 100]
+  expect(result).toEqual(expected)
 });

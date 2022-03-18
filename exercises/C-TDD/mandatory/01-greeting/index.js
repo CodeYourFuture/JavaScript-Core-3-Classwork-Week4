626fb9dc723f5ac36bec672ec1a2cdb79e5edb10
Monika39-maker <dangolmonika@yahoo.com>
2022-03-18 00:46:13 +0000

    all tests passed locally

--- exercises/C-TDD/mandatory/01-greeting/index.js
+++ exercises/C-TDD/mandatory/01-greeting/index.js
@@ -1,1 +1,13 @@
-const greet = () => {}
+const greet = (name) => {
+    if (Array.isArray(name) && name.length === 2) {
+        return `Hello, ${name[0]} and ${name[1]}.`
+    } else if (name === null) {
+        return "Hello, my friend."
+    } else if (name === name.toUpperCase()) {
+        return `HELLO ${name}!`
+    }
+    
+    return `Hello, ${name}.`
+}
+
+module.exports =  greet

3786f62b72f402a6b86f7e090c68680e3bacad2a
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-18 23:51:17 +0000

    Add class work week 4 exercises

--- exercises/C-TDD/mandatory/01-greeting/index.js
+++ exercises/C-TDD/mandatory/01-greeting/index.js
@@ -1,1 +1,20 @@
-const greet = () => {}
+const greet = (name) => {
+  if (name === null) return `Hello, my friend.`;
+  if (name instanceof Array && name.length > 2) {
+    let print = "Hello";
+
+    for (let i = 0; i < name.length - 1; i++) {
+      print += name[i] === name.length - 1 ? `${name[i]}` : `, ${name[i]}`;
+    }
+    print += `, and ${name[name.length - 1]}.`;
+    return print;
+  }
+
+  if (name instanceof Array) {
+    return `Hello, ${name[0]} and ${name[1]}.`;
+  }
+  if (name.toUpperCase() === name) return `HELLO ${name}!`;
+  return `Hello, ${name}.`;
+};
+
+module.exports = greet;

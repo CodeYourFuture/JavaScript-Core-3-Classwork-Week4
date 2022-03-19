e3250ef580fecc3334fad3f2e84099ff1f68f6fa
Timea <timea.reich@gmail.com>
2022-03-19 01:31:59 +0000

    updated B-write-tests

--- exercises/B-write-tests/mandatory/02-remove-vowels/remove-vowels.test.js
+++ exercises/B-write-tests/mandatory/02-remove-vowels/remove-vowels.test.js
@@ -3,6 +3,7 @@ const removeVowels = require("./remove-vowels");
 /* Complete the tests */
 
 test("remove vowels from word", () => {
+  expect(removeVowels('Billy')).toEqual('i')
   // Arrange
   // Act
   // Assert

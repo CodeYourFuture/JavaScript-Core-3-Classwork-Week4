6d936b4a40d84962c3b9a0a79b9097e9545dbf77
SenamiAhifon <89362897+SenamiAhifon@users.noreply.github.com>
2022-03-19 15:59:29 +0000

    fizzbuzz

--- exercises/C-TDD/extra/02-fizz-buzz/fizz-buzz.js
+++ exercises/C-TDD/extra/02-fizz-buzz/fizz-buzz.js
@@ -1,21 +1,15 @@
 function fizzbuzz(n) {
   /* Implement the function to make the tests pass */
   let result = "";
-
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      return result += " FizzBuzz";
-    } else if (i % 5 === 0) {
-      return result += " Buzz";
-    } else if (i % 3 === 0) {
-      return result += " Fizz";
-    } else {
-      result += " " + i ;
-    }
-   
+  for (i = 1; i <= n; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      result += " FizzBuzz";
+      continue;
+    } else if (i % 3 === 0) result += " Fizz";
+    else if (i % 5 === 0) result += " Buzz";
+    else result += " " + i;
   }
-   return result.trim();
+  return result.trim();
 }
-
 
 module.exports = fizzbuzz;

9cb3c5941a043298550168f1725e114aa3ab8b91
Timea <timea.reich@gmail.com>
2022-03-19 01:30:26 +0000

    mandatory done

--- exercises/A-make-tests-pass/extra/03-sum-numbers/sum-numbers.js
+++ exercises/A-make-tests-pass/extra/03-sum-numbers/sum-numbers.js
@@ -1,3 +1,4 @@
 module.exports = function sumNumbers(numbers) {
+  return numbers.reduce((a, b) => a + b);
   /* Implement the function to make the tests pass */
-}
+};
--- exercises/C-TDD/mandatory/01-greeting/index.js
+++ exercises/C-TDD/mandatory/01-greeting/index.js
@@ -1,1 +1,13 @@
-const greet = () => {}
+function greet(name) {
+  if (name === null || name === "") {
+    return "Hello, my friend.";
+  } else if (name === name.toUppercase()) {
+    return `HELLO, ${name}!`;
+  } else if (Array.isArray(name)) {
+    return `Hello, ${name[0]} and ${name[1]}.`;
+  } else {
+    return `Hello, ${name}.`;
+  }
+}
+
+module.exports = greet;

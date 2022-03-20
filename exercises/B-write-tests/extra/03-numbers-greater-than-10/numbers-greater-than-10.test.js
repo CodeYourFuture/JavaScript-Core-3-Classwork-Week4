cc26b30090d5967fe2ba0ff69467787ccf91e53b
Ali Jahankah <aral2@yahoo.com>
2022-03-20 20:47:16 +0000

    Complete

--- exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
+++ exercises/B-write-tests/extra/03-numbers-greater-than-10/numbers-greater-than-10.test.js
@@ -3,6 +3,10 @@ const numbersGreaterThan10 = require("./numbers-greater-than-10");
 /* Complete the tests */
 
 test("Get numbers greater than 10", () => {
+  const input = [5, 6, 8, 22, 55, 88, 2, 7];
+  const expectedResult = [22, 55, 88];
+  const output = numbersGreaterThan10(input);
+  expect(output).toEqual(expectedResult);
   // Arrange
   // Act
   // Assert
--- exercises/C-TDD/extra/02-fizz-buzz/fizz-buzz.js
+++ exercises/C-TDD/extra/02-fizz-buzz/fizz-buzz.js
@@ -1,10 +1,18 @@
 function fizzbuzz(n) {
   const arr = [];
-  for (let i = 1; i <= n; i++) {
-    i % 3 === 0 ? arr.push("fizz") : i % 5 === 0 ? arr.push("buzz") : arr.push(i)
 
-  };
-  return arr
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
 }
 
 module.exports = fizzbuzz;
